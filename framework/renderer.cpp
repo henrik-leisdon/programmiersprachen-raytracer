557ce0fb144a1750dc4db5a6acfe9bc818ac6ccc
Vanessa Retz <vanessa.retz@aol.com>
2018-08-25 18:26:31 +0200

    small changes

--- framework/renderer.cpp
+++ framework/renderer.cpp
@@ -28,12 +28,12 @@ void Renderer::render(Scene& scene, int frame)
   for (int y=0; y < height_; ++y) {
     for (int x = 0; x < width_; ++x) {
       Pixel p(x,y);
-      double d = (width_ / 2) / tan(51.5 / 2 * M_PI / 180);
+     // double d = (width_ / 2) / tan(51.5 / 2 * M_PI / 180);
       
       float distance = 100;
       glm::vec3 origin{400.0f, 300.0f ,0.0f};
       glm::vec3 direction{x-origin.x,y-origin.y, -100.0f};
-      direction = direction + glm::vec3{ x - (0.5 * width_),y - (0.5 * height_),d};
+      //direction = direction + glm::vec3{ x - (0.5 * width_),y - (0.5 * height_),d};
       Ray ray{origin,glm::normalize(direction)};
       
       if ( ((x/checker_pattern_size)%2) != ((y/checker_pattern_size)%2)) {
@@ -43,7 +43,7 @@ void Renderer::render(Scene& scene, int frame)
       }
 
       float cam_dist = 20000;
-      //shared_ptr<Shape> nearest_obj;
+      shared_ptr<Shape> nearest_obj;
       
       for(auto i : scene.shape_vec)
       {
@@ -54,10 +54,14 @@ void Renderer::render(Scene& scene, int frame)
           if(cam_dist>distance)
           {
             cam_dist = distance;
+            i = nearest_obj;
+            Hit hitp(true, cam_dist,  ;
           }
           p.color = i->getMaterial()->ka_;
         }
       }
+
+
 
       //p.color = getPixelColor(nearest_obj, ray, scene);
       
@@ -69,11 +73,19 @@ void Renderer::render(Scene& scene, int frame)
   ppm_.save(filename_);
 }
 
-Color Renderer::getPixelColor(std::shared_ptr<Shape> Object, Ray& ray, Scene& scene)
+Color Renderer::getPixelColor(std::shared_ptr<Shape> nearest_obj, Ray& ray, Scene& scene)
 {   
     
     
 }
+
+/*Color Renderer::shadow(std::shared_ptr<Shape> Object, Ray& ray, Scene& scene){
+    color.r += mat.kd_.r * (*it).ld_.r * (glm::dot(camHit.normvec_, lightRay.direction) + mat.ks_.r * r_v_);
+    color.g += mat.kd_.g * (*it).ld_.g * (glm::dot(camHit.normvec_, lightRay.direction) + mat.ks_.g * r_v_);
+    color.b += mat.kd_.b * (*it).ld_.b * (glm::dot(camHit.normvec_, lightRay.direction) + mat.ks_.b * r_v_);
+            
+}*/
+
 
 void Renderer::write(Pixel const& p)
 {
